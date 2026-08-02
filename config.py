94cf503f6970dbc9bc607e4a44bf8d06075e4f7c
Egor Cheredov <egorceredov@gmail.com>
2026-08-03 00:07:18 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,1 @@
-import os
 
-# Основные доступы из твоего запроса
-BOT_TOKEN = "8943003984:AAGfxgrY5LVo_tvbo3xd1HV68FnMyqAbL_4"
-API_ID = 38402967
-API_HASH = "3115aeb3609ddb322ebf80d62462fb3c"
-DB_URL = "sqlite+aiosqlite:///local_base.db"
-ADMIN_ID = 2092877628
-
-# Лимиты для премиального UI
-MAX_KEYWORDS = 5
